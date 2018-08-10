4805e1de1d6c972848c512bacf5908947d73e6cb
samih <samih1079@gmail.com>
2018-08-11 01:06:01 +0300

    ..

--- src/main/scala/MainApptst.scala
+++ src/main/scala/MainApptst.scala
@@ -1,11 +1,11 @@
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions._
 object MainApptst extends App {
-  print("jjjj")
   val spark = SparkSession.builder().master("local").appName("test1").getOrCreate();
   spark.sparkContext.setLogLevel("ERROR")
-  val peopleDFCsv = spark.read.
-    format("csv")
+  val peopleDFCsv = spark
+    .read
+    .format("csv")
     .option("sep", ",")
     .option("inferSchema", "true")
     .option("header", "true")
@@ -16,8 +16,13 @@ object MainApptst extends App {
   import spark.implicits._
   val qeury="select count(result),class1,class2 from progs group by (class1,class2)"
 
-  val res=peopleDFCsv.groupBy("class1","class2","result","program1","program2").agg(count("result"))
-  res.show()
+  val res=peopleDFCsv
+    .filter("result != 'NULL'")
+    .orderBy("program1","program2")
+    .groupBy("class1","class2","result","program1","program2")
+    .agg(count("result"))
+
+  res.show(100)
 
 
 }
