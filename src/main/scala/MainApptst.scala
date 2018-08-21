641cd4f9ebfa7ad6e89650f2a6b2bcb214769e5a
samih <samih1079@gmail.com>
2018-08-21 03:26:18 +0300

    1.organized  by method 2. add col usage of 'use' percentage

--- src/main/scala/MainApptst.scala
+++ src/main/scala/MainApptst.scala
@@ -1,34 +1,66 @@
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions._
+import SPLETools._
 
 object MainApptst extends App {
-  val spark = SparkSession.builder().master("local").appName("test1").getOrCreate();
-  spark.sparkContext.setLogLevel("ERROR")
-  val peopleDFCsv = spark
-    .read
-    .format("csv")
-    .option("sep", ",")
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .load("debugInfo_1533200012911fxd.csv").toDF()
-
-  //peopleDFCsv.createOrReplaceTempView("progs")
-
+  val spark =intitSpark()
+  val peopleDFCsv:DataFrame =read("debugInfo_1533200012911fxd.csv")
   import spark.implicits._
+  val res=countResultNoNullForEachProg(peopleDFCsv)
 
-  val res = peopleDFCsv
-    //.filter("result != 'NULL'")
-    .filter(col("result") =!= ("NULL"))
-    .orderBy("program1", "program2", "class1", "class2")
-    .groupBy("program1", "program2", "class1", "class2", "result")
-    .agg(count("result"))
+  showClassesResultForEachProgPair(res)
 
+  // SparkSession.builder().master("local").appName("test1").getOrCreate();
+  //spark.sparkContext.setLogLevel("ERROR")
+//  val peopleDFCsv = spark
+//    .read
+//    .format("csv")
+//    .option("sep", ",")
+//    .option("inferSchema", "true")
+//    .option("header", "true")
+//    .load("debugInfo_1533200012911fxd.csv").toDF()
+
+
+//  val res = peopleDFCsv
+//    //.filter("result != 'NULL'")
+//    .filter(col("result") =!= ("NULL"))
+//    .orderBy("program1", "program2", "class1", "class2")
+//    .groupBy("program1", "program2", "class1", "class2", "result")
+//    .agg(count("result"))
+
+
+
+//  private var progs: Dataset[Row] = res.select("program1", "program2")
+//    .orderBy("program1", "program2").distinct()
+//  var progs:DataFrame=allProgsDistinctPairs(res)
+//  val progsCount = progs.count()
+//  val batchSize = 10
+//  val iterations = Math.max(Math.ceil(progsCount / batchSize) , 1).intValue()
+//
+//  for (i <- 1 to iterations) {
+//    println(s"iter:$i")
+//    val tmpDf = progs.limit(batchSize)
+//    val tmpVals = tmpDf.collect()
+//    tmpVals.foreach(r => {
+//      println(r.getString(0) + " " + r.getString(1))
+//      val classes = res
+//        .select("*")
+//        .filter("program1='" + r.getString(0) + "'" + " AND program2='" + r.getString(1) + "'")
+//      classes.withColumn("use", when($"result" === "USE", $"count(result)").otherwise(0))
+//        .withColumn("overloading", when($"result" === "OVERLOADING", $"count(result)").otherwise(0))
+//        .withColumn("extension", when($"result" === "EXTENSION", $"count(result)").otherwise(0))
+//        .withColumn("refine_extension", when($"result" === "REFINED_EXTENSION", $"count(result)").otherwise(0))
+//        .groupBy("program1", "program2", "class1", "class2")
+//        .sum("use", "overloading", "extension","refine_extension")
+//        .show()
+//
+//    })
+//    progs = progs.except(tmpDf)
+//  }
 
   //res.show(100);
   // private val value: Dataset[Row] = res.select("count(result)").filter("program1='" + "P14" + "'" + " AND program2='" + "P15" + "'")
   // value.show()
-  private var progs: Dataset[Row] = res.select("program1", "program2")
-    .orderBy("program1", "program2").distinct()
   //  progs.foreach(row=>{
   //    println(row.getString(0)+" "+row.getString(1))
   //  })
@@ -42,30 +74,7 @@ object MainApptst extends App {
   //    .sum("use","overloading","extension")
   //    .show(100)
   //
-  val progsCount = progs.count()
-  val batchSize = 10
-  val iterations = Math.max(Math.ceil(progsCount / batchSize) , 1).intValue()
 
-  for (i <- 1 to iterations) {
-    println(s"iter:$i")
-    val tmpDf = progs.limit(batchSize)
-    val tmpVals = tmpDf.collect()
-    tmpVals.foreach(r => {
-      println(r.getString(0) + " " + r.getString(1))
-      val classes = res
-        .select("*")
-        .filter("program1='" + r.getString(0) + "'" + " AND program2='" + r.getString(1) + "'")
-      classes.withColumn("use", when($"result" === "USE", $"count(result)").otherwise(0))
-        .withColumn("overloading", when($"result" === "OVERLOADING", $"count(result)").otherwise(0))
-        .withColumn("extension", when($"result" === "EXTENSION", $"count(result)").otherwise(0))
-        .withColumn("refine_extension", when($"result" === "REFINED_EXTENSION", $"count(result)").otherwise(0))
-        .groupBy("program1", "program2", "class1", "class2")
-        .sum("use", "overloading", "extension","refine_extension")
-        .show()
-
-    })
-    progs = progs.except(tmpDf)
-  }
 
 
   //  print("hiii")
---
+++ src/main/scala/SPLETools.scala
@@ -0,0 +1,86 @@
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions._
+
+
+ object SPLETools {
+   var spark:SparkSession = SparkSession.builder().master("local").appName("test1").getOrCreate()
+
+   def intitSpark():SparkSession= {
+    spark
+   }
+
+
+  def read(csvFile:String): DataFrame =
+  {
+    spark.sparkContext.setLogLevel("ERROR")
+    val peopleDFCsv = spark
+      .read
+      .format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load(csvFile).toDF()
+    peopleDFCsv
+  }
+   def countResultNoNullForEachProg(dataFrame: DataFrame):DataFrame =
+   {
+     dataFrame
+     //.filter("result != 'NULL'")
+     .filter(col("result") =!= ("NULL"))
+     .orderBy("program1", "program2", "class1", "class2")
+     .groupBy("program1", "program2", "class1", "class2", "result")
+     .agg(count("result"))
+   }
+   def allProgsDistinctPairs(dataFrame: DataFrame): DataFrame =
+   {
+     dataFrame.select("program1", "program2")
+       .orderBy("program1", "program2").distinct()
+   }
+
+   def showClassesResultForEachProgPair(allClassRes:DataFrame): Unit ={
+     var progs=allProgsDistinctPairs(allClassRes);
+     val progsCount = progs.count()
+     val batchSize = 10
+     val iterations = Math.max(Math.ceil(progsCount / batchSize) , 1).intValue()
+
+     for (i <- 1 to iterations) {
+       println(s"iter:$i")
+       val tmpDf = progs.limit(batchSize)
+       val tmpVals = tmpDf.collect()
+       tmpVals.foreach(r => {
+         println(r.getString(0) + " " + r.getString(1))
+         classesResEachProgPair(r.getString(0),r.getString(1),allClassRes).show();
+//         val classes = allClassRes
+//           .select("*")
+//           .filter("program1='" + r.getString(0) + "'" + " AND program2='" + r.getString(1) + "'")
+//         classes.withColumn("use", when(col("result") === "USE", col("count(result)")).otherwise(0))
+//           .withColumn("overloading", when(col("result") === "OVERLOADING", col("count(result)")).otherwise(0))
+//           .withColumn("extension", when(col("result") === "EXTENSION", col("count(result)")).otherwise(0))
+//           .withColumn("refine_extension", when(col("result") === "REFINED_EXTENSION", col("count(result)")).otherwise(0))
+//           .groupBy("program1", "program2", "class1", "class2")
+//           .sum("use", "overloading", "extension","refine_extension")
+//           .show()
+
+       })
+       progs = progs.except(tmpDf)
+     }
+   }
+
+   def classesResEachProgPair(prog1:String,prog2:String, allClassRes:DataFrame):DataFrame={
+     allClassRes
+       .select("*")
+       .filter("program1='" + prog1 + "'" + " AND program2='" + prog2 + "'")
+       .withColumn("use", when(col("result") === "USE", col("count(result)")).otherwise(0))
+       .withColumn("overloading", when(col("result") === "OVERLOADING", col("count(result)")).otherwise(0))
+       .withColumn("extension", when(col("result") === "EXTENSION", col("count(result)")).otherwise(0))
+       .withColumn("refine_extension", when(col("result") === "REFINED_EXTENSION", col("count(result)")).otherwise(0))
+       .groupBy("program1", "program2", "class1", "class2")
+       .sum("use", "overloading", "extension","refine_extension")
+       .withColumn("relationSum",
+         col("sum(use)")/(col("sum(use)")+col("sum(overloading)")+col("sum(extension)")+col("sum(refine_extension)")))
+
+   }
+
+
+
+}
